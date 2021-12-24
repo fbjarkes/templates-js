51672414d8adb5bb1b9ba374b689a4a3002a00bf
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2021-12-24 18:51:54 +0100

    Spaces formatting

--- run.js
+++ run.js
@@ -4,11 +4,10 @@ const dotenv = require('dotenv');
 const logger = require('./logger');
 
 const main = async () => {
-	// eslint-disable-next-line no-undef
-	const { parsed: cfg } = dotenv.config({ path: `${__dirname}/.env` });
+    // eslint-disable-next-line no-undef
+    const { parsed: cfg } = dotenv.config({ path: `${__dirname}/.env` });
     logger.info(`Verbose: ${argv.verbose}`);
-	logger.info(`Using config ${JSON.stringify(cfg)}`)
+    logger.info(`Using config ${JSON.stringify(cfg)}`);
 };
 
 main();
-
