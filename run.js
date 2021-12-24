d938d1ea8c5c5504cd523aa2615c0de6ebf86fa1
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2021-12-24 17:50:41 +0100

    husky test

--- run.js
+++ run.js
@@ -7,7 +7,7 @@ const main = async () => {
 	// eslint-disable-next-line no-undef
 	const { parsed: cfg } = dotenv.config({ path: `${__dirname}/.env` });
     logger.info(`Verbose: ${argv.verbose}`);
-	logger.info(`Using config config ${JSON.stringify(cfg)}`)
+	logger.info(`Using config ${JSON.stringify(cfg)}`)
 };
 
 main();
